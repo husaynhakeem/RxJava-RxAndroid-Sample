5ad2123a1e43c952f308b771e99b258b8de3ff31
Husayn Hakeem <husaynhakeem@gmail.com>
2017-07-04 00:36:02 +0100

    Initial commit

---
+++ app/src/main/java/husaynhakeem/io/android_retrofit2_sample/MainActivity.java
@@ -0,0 +1,13 @@
+package husaynhakeem.io.android_retrofit2_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
